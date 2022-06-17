3445224d1ae4858e978b0c8ab32b07c42471b171
Nam <50343159+namhkse@users.noreply.github.com>
2022-06-17 12:19:16 +0700

    Fix counter time

--- OnlineLearning/web/js/question.js
+++ OnlineLearning/web/js/question.js
@@ -87,7 +87,7 @@ function initQuestion() {
                 counter = setInterval(() => {
                     let distance = endTime - new Date();
 
-                    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+                    var hours = Math.floor(distance / (1000 * 60 * 60));
                     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
                     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
